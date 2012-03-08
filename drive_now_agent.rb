24bc32b82aee29a77f39006d228f08c08fa382c4
Simon Woker <github@simonwoker.de>
2012-03-08 01:07:30 +0100

    initial implementation

---
+++ drive_now_agent.rb
@@ -0,0 +1,25 @@
+﻿class DriveNowAgent
+	require 'rubygems'
+	require 'open-uri'
+	require 'json'
+	$KCODE = "U"
+
+	attr_reader :cars
+
+	def initialize(options = {})
+		options = { :city => :duesseldorf,
+			    :uri => 'https://www.drive-now.com/php/metropolis/vehicle_filter' }.merge(options)
+
+		page = open(options[:uri]).read
+		cars = JSON(page)["rec"]["vehicles"]["vehicles"]
+		cars = cars.select { |item| item["cit"] == DriveNowAgent.cities[options[:city]] }
+		cars.map! { |item| DriveNowCar.new(item) }
+		@cars = cars
+	end
+	
+	def self.cities
+		{:duesseldorf => "1293",
+		:berlin => "6099", 
+		:muenchen => "4604"}
+	end
+end
---
+++ drive_now_car.rb
@@ -0,0 +1,39 @@
+﻿class DriveNowCar
+	attr_accessor :json
+
+	def initialize(json_param)
+		@json = json_param
+	end
+	
+	def name
+		@name ||= "#{json['personalName']} (#{json['color']})"
+	end
+	
+	def address
+		@address ||= json['address']
+	end
+	
+	def automatic?
+		@automatic ||= json['auto'] == 'Y'
+	end
+	
+	def license_plate
+		@license ||= json['licensePlate'].gsub('  ', '')
+	end
+	
+	def model
+		@model ||= json['model']
+	end
+	
+	def position
+		@position ||= json['position']
+	end
+
+	def fuel_state
+		@fuel_state ||= "#{json['fuelState']}%"
+	end
+
+	def clean
+		@clean ||= json['innerCleanliness']
+	end
+end
